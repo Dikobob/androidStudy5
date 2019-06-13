38b939d0988464466638ce8d69a4937e34943aad
borisdidenko <dikobob@gmail.com>
2019-06-13 13:15:13 +0300

    stage 30 is DONE но что-то не так, страницы не грузит =(((!

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
@@ -197,7 +197,8 @@ public class PhotoGalleryFragment extends VisibleFragment {
 
         @Override
         public void onClick(View view) {
-            Intent i = new Intent(Intent.ACTION_VIEW, mGalleryItem.getPhotoPageUri());
+            Intent i = PhotoPageActivity
+                    .newIntent(getActivity(), mGalleryItem.getPhotoPageUri());
             startActivity(i);
         }
     }
